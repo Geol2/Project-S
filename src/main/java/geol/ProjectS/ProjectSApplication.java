3b493aec3071f3366034d8eb1d828938a4e92717
geol2 <big9401@gmail.com>
2021-07-10 17:06:05 +0900

    SpringBoot init

---
+++ src/main/java/geol/ProjectS/ProjectSApplication.java
@@ -0,0 +1,13 @@
+package geol.ProjectS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/geol/ProjectS/ProjectSApplicationTests.java
@@ -0,0 +1,13 @@
+package geol.ProjectS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
