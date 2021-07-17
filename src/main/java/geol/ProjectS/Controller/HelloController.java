fdde2688f067f748a18ec8b0037b2e10c99003f2
geol2 <big9401@gmail.com>
2021-07-17 20:25:07 +0900

    - MVC 및 템플릿 엔진 - API 작동 방식 (JSON) - 회원 관련 리포지토리 만들기 및 테스트 케이스 작성 확인

--- src/main/java/geol/ProjectS/Controller/HelloController.java
+++ src/main/java/geol/ProjectS/Controller/HelloController.java
@@ -3,6 +3,8 @@ package geol.ProjectS.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,35 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "Hello!!");
         return "hello"; // 해당 템플릿 로드 .html
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam(value = "name", required = false) String name) {
+        return "hello " + name;
+    }
+
+    static class Hello {
+        private String name;
+
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getName() {
+            return name;
+        }
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam(value = "name", required = false) String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
     }
 }
